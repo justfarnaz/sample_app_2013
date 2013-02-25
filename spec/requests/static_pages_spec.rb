cdf701fc14b7da9d0d7df4b51e16ee2e676e1aeb
Farnaz Bakhtari <justfarnaz@gmail.com>
2013-02-24 22:38:24 -0500

    finished Static Pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home Page" do
+    it "Should have content 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+    it "should have right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Ruby on Rails | Home")
+    end
+  end
+  
+ describe "Help Page" do
+   it "Should have 'Help page' content" do 
+     visit "/static_pages/help"
+     page.should have_selector('h1', :text => 'help page')
+   end  
+   it "should have right title" do
+     visit '/static_pages/help'
+     page.should have_selector('title', :text => "Ruby on Rails | Help")
+   end
+ end
+ 
+ describe "About Page" do
+   it "Should have 'About US' in its content" do
+     visit "/static_pages/about"
+     page.should have_selector('h1', :text => 'About Us')
+   end   
+   it "should have right title" do
+     visit '/static_pages/about'
+     page.should have_selector('title', :text => "Ruby on Rails | About")
+   end
+ end
+ 
+  
+end
