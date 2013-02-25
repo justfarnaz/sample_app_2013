e60222f50f901698589b63df59d850e08621deca
Farnaz Bakhtari <justfarnaz@gmail.com>
2013-02-25 00:46:57 -0500

    added the let command

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let(:base_title) {"Ruby on Rails | "}
   describe "Home Page" do
     it "Should have content 'Sample App'" do
       visit '/static_pages/home'
@@ -8,7 +9,7 @@ describe "StaticPages" do
     end
     it "should have right title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Ruby on Rails | Home")
+      page.should have_selector('title', :text => "#{base_title}Home")
     end
   end
   
@@ -19,7 +20,7 @@ describe "StaticPages" do
    end  
    it "should have right title" do
      visit '/static_pages/help'
-     page.should have_selector('title', :text => "Ruby on Rails | Help")
+     page.should have_selector('title', :text => "#{base_title}Help")
    end
  end
  
@@ -30,7 +31,7 @@ describe "StaticPages" do
    end   
    it "should have right title" do
      visit '/static_pages/about'
-     page.should have_selector('title', :text => "Ruby on Rails | About")
+     page.should have_selector('title', :text => "#{base_title}About")
    end
  end
  
